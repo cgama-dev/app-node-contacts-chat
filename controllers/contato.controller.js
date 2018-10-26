b4b9cf8539ad21e3bbc52e9b918c1c4354e8d746
Cleyton Gama <cleytongama@gmail.com>
2018-10-25 23:17:48 -0300

    Atualizando view de contatos

--- controllers/contato.controller.js
+++ controllers/contato.controller.js
@@ -12,20 +12,49 @@ module.exports = (() => {
 
             res.render('contatos/index', params)
         },
-        show: (req, res) => {
-            res.send('show cotnatos')
-        },
         create: (req, res) => {
-            res.send('show criação de usuários')
+            const contato = {
+                email: req.body.email,
+                nome: req.body.nome
+            }
+            const usuario = req.session.usuario
+
+            usuario.contatos = [...usuario.contatos, contato]
+
+            res.redirect('/contatos')
+        },
+        show: (req, res) => {
+            const id = req.body.id
+            const contato = req.session.usuario.contatos[id]
+            const params = { contato: contato, id: id }
+
+            res.render('contatos/show', params)
         },
         edit: (req, res) => {
-            res.send('edição de usuários')
+            const id = req.params.id
+            const usuario = req.session.usuario
+            const contato = usuario.contatos[id]
+            const params = {
+                usuario: usuario
+                , contato: contato
+                , id: id
+            };
+
+            res.render('contatos/edit', params);
         },
         update: (req, res) => {
-            res.send('atualizando usuários')
+            const contato = req.body.contato
+            const usuario = req.session.usuario;
+            usuario.contatos[req.params.id] = contato;
+
+            res.redirect('/contatos');
         },
         destroy: (req, res) => {
-            res.send('apagando usuários')
+            const usuario = req.body.usuario
+            const id = req.params.id
+            
+            usuario.contatos.splice(id, 1)
+            res.redirect('/contatos');
         }
     }
 
